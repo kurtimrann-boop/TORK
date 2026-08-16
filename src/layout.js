c6fe365379ad243b5dd463174c875415ed2287a8
kurtimrann-boop <kurtimrann@gmail.com>
2026-08-16 15:38:11 +0300

    Update layout.js

--- src/layout.js
+++ src/layout.js
@@ -1,8 +1,28 @@
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Tork | Akıllı Navlun Platformu",
+  description:
+    "Tork, yük verenler ve nakliyeciler için akıllı navlun ve lojistik operasyon platformudur.",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="tr">
+    <html
+      lang="tr"
+      className={`${geistSans.variable} ${geistMono.variable}`}
+    >
       <body>{children}</body>
     </html>
   );
